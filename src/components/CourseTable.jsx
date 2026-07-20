52376afcb6bab8eb872d097731ce5a841b13a90b
tammirsidevika <tammirsidevika05@gmail.com>
2026-07-20 19:55:20 +0530

    Update CourseTable.jsx

--- src/components/CourseTable.jsx
+++ src/components/CourseTable.jsx
@@ -1,12 +1,28 @@
 import { FaEdit, FaTrash } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./CourseTable.css";
 
 function CourseTable({ courses = [] }) {
+  const navigate = useNavigate();
+
+  const deleteCourse = async (id) => {
+    try {
+      await axios.delete(
+        `https://coursetracker-bd.onrender.com/api/courses/${id}`
+      );
+
+      alert("Course Deleted Successfully");
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+      alert("Delete Failed");
+    }
+  };
+
   return (
     <div className="table-container">
-
       <table className="course-table">
-
         <thead>
           <tr>
             <th>Course Name</th>
@@ -20,7 +36,6 @@ function CourseTable({ courses = [] }) {
         </thead>
 
         <tbody>
-
           {courses.length === 0 ? (
             <tr>
               <td colSpan="7" className="no-data">
@@ -38,22 +53,25 @@ function CourseTable({ courses = [] }) {
                 <td>{course.progress}%</td>
 
                 <td>
-                  <button className="edit-btn">
+                  <button
+                    className="edit-btn"
+                    onClick={() => navigate(`/edit-course/${course._id}`)}
+                  >
                     <FaEdit />
                   </button>
 
-                  <button className="delete-btn">
+                  <button
+                    className="delete-btn"
+                    onClick={() => deleteCourse(course._id)}
+                  >
                     <FaTrash />
                   </button>
                 </td>
               </tr>
             ))
           )}
-
         </tbody>
-
       </table>
-
     </div>
   );
 }
